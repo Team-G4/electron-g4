549a0c3c52e6dbd4f85244585f29009fb4a0687d
Devin <30705254+HiItsDevin@users.noreply.github.com>
2019-12-09 23:56:17 -0800

    BOLD TO ASSUME THAT I KNOW WHAT I'M DOING
    SPOILER ALERT: I DON'T

---
+++ scripts/build-scripts/build-linux.sh
@@ -0,0 +1,2 @@
+cd ~/G4X/
+npm i -g electron-packager && electron-packager ~//G4X G4X.7 --icon=~//G4X//res//icon.ico --executable-name=G4X.7 --appname=G4X.7 --app-version=4.7.0 --ignore=.gitignore --ignore=README.md --ignore=CNAME --ignore=service-worker.js --ignore=G4X.iml --ignore=/.idea --ignore=/node_modules --overwrite && npm start && pause
