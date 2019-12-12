61442b4b14f3026dd6b097e44e42290f3b6337aa
scintilla4evr <kacper17@outlook.com>
2019-12-12 18:36:51 +0100

    Reverted some stuff

--- main.js
+++ main.js
@@ -59,7 +59,9 @@ function initIpcEvents(win) {
 
 app.commandLine.appendSwitch("autoplay-policy", "no-user-gesture-required");
 
-createWindow();
+app.on("ready", () => {
+  createWindow();
+})
 
 app.on('browser-window-created', (e, window) => {
   window.setMenu(null);
--- test.js
+++
@@ -1,5 +0,0 @@
-const { app } = require('electron');
-
-app.on("ready", () => {
-    require("./main")
-})
